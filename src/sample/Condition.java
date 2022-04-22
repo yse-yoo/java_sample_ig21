03cb000a7f5794b3fd354a1825defdf2896706dd
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-04-22 14:31:59 +0900

    update

--- src/sample/Condition.java
+++ src/sample/Condition.java
@@ -3,23 +3,31 @@ package sample;
 public class Condition {
 
 	public static void main(String[] args) {
-		//距離(km)
-		int distance = 3;
+		// 距離(km)
+		float distance = 0.5f;
 
-		//移動手段
+		// 移動手段
 		String movingMethod;
 
-		if (distance >= 5) {
-			//5km以上の場合
-			movingMethod = "電車";
-		} else if (distance >= 1 && distance < 5) {
-			//1km以上、5km未満の場合
-			movingMethod = "自転車";
+		boolean isRain = true;
+
+		if (isRain) {
+			// 雨だったら
+			movingMethod = "家にいます";
 		} else {
-			//それ以外
-			movingMethod = "徒歩";
+			// 雨でなければ
+			if (distance >= 5) {
+				// 5km以上の場合
+				movingMethod = "電車";
+			} else if (distance >= 1 && distance < 5) {
+				// 1km以上、5km未満の場合
+				movingMethod = "自転車";
+			} else {
+				// それ以外
+				movingMethod = "徒歩";
+			}
 		}
-		
+
 		System.out.println(movingMethod);
 
 	}

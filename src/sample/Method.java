e49e84107ee96158025b8d7b7a95c426e59fb1b7
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-05-13 11:49:21 +0900

    add method

--- src/sample/Method.java
+++ src/sample/Method.java
@@ -5,10 +5,28 @@ public class Method {
 	public static void main(String[] args) {
 		Float answer = calculate(5f);
 		System.out.println(answer);
+
+		answer = calculate(-3f);
+		System.out.println(answer);
+
+		//staticでないメソッドは直接実行できない
+//		answer = calculate2(-3f);
+//		System.out.println(answer);
+	}
+	
+	public static int calculateTotalPrice(int price, int amount) {
+		int totalPrice = price * amount;
+		return totalPrice;
 	}
 
 	public static Float calculate(Float x) {
 		Float y = x + 3;
 		return y;
 	}
+
+	public Float calculate2(Float x) {
+		Float y = x + 3;
+		return y;
+	}
+
 }

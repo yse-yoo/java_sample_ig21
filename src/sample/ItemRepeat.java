a74f0d592ced251c9701e401735d9aa32359289b
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-05-13 10:34:51 +0900

    add for

--- src/sample/ItemRepeat.java
+++ src/sample/ItemRepeat.java
@@ -7,6 +7,14 @@ public class ItemRepeat {
 		for (int i = 0; i <= 10; i++) {
 			System.out.println(i);
 		}
+		
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <= 10; j++) {
+				String plot = "(" + i + "," + j + ")";
+				System.out.print(plot);
+			}
+			System.out.println();
+		}
 	}
 
 }

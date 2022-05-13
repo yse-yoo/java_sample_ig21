8635c9f0157a5111411ecd75812751d65f2262ef
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-05-13 09:32:14 +0900

    add Drink.java

---
+++ src/sample/Drink.java
@@ -0,0 +1,19 @@
+package sample;
+
+import java.util.Arrays;
+
+public class Drink {
+
+	public static void main(String[] args) {
+		String[] drinks = { "コーヒー", "紅茶", "ほうじ茶" };
+		System.out.println(drinks);
+		System.out.println(Arrays.toString(drinks));
+		
+		int[] numbers = new int[3];
+		numbers[0] = 55;
+		numbers[1] = 32;
+		numbers[2] = 72;
+		System.out.println(Arrays.toString(numbers));
+	}
+
+}

f37a2e0d402f881d82cad31d6253eea00a39ad22
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-05-13 10:59:10 +0900

    add 9x9

--- src/sample/ItemRepeat.java
+++ src/sample/ItemRepeat.java
@@ -15,6 +15,15 @@ public class ItemRepeat {
 			}
 			System.out.println();
 		}
+		
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				int answer = i * j;
+				String message = answer + " ";
+				System.out.print(message);
+			}
+			System.out.println();
+		}
 	}
 
 }

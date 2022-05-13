ea2b4081cc3c8597d2962139b5d6ee400fb0c561
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-05-13 10:21:21 +0900

    add ItemRepeat

---
+++ src/sample/ItemRepeat.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class ItemRepeat {
+
+	public static void main(String[] args) {
+		//forを入力して Ctrl + スペース	
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(i);
+		}
+	}
+
+}

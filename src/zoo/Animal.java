cb24163ed7eb9c957c0baeb2251f3ab5e5510281
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-05-20 13:45:13 +0900

    add animal constructor

--- src/zoo/Animal.java
+++ src/zoo/Animal.java
@@ -1,6 +1,12 @@
 package zoo;
 
 public class Animal {
+	public Animal(String type, String name, String crying) {
+		this.type = type;
+		this.name = name;
+		this.crying = crying;
+	}
+
 	//種別
 	public String type;
 
--- src/zoo/AnimalApp.java
+++ src/zoo/AnimalApp.java
@@ -3,19 +3,8 @@ package zoo;
 public class AnimalApp {
 
 	public static void main(String[] args) {
-		Animal dog = new Animal();
-		dog.type = "dog";
-		dog.name = "タロー";
-		dog.crying = "わん！";
-		
-//		System.out.println(dog.type);
-//		System.out.println(dog.name);
-//		System.out.println(dog.crying);
-		
-		Animal cat = new Animal();
-		cat.type = "cat";
-		cat.name = "ミケ";
-		cat.crying = "にゃー！";
+		Animal dog = new Animal("dog", "タロー", "わん！");
+		Animal cat = new Animal("cat", "ミケ", "にゃー！");
 				
 		dog.walk();
 		dog.cry();

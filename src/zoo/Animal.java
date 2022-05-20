6bf29870f651b4d3eacf6fe50738b4e37642689c
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-05-20 12:02:17 +0900

    add animal method

--- src/zoo/Animal.java
+++ src/zoo/Animal.java
@@ -9,4 +9,18 @@ public class Animal {
 
 	//鳴き声
 	public String crying;
+	
+	public void walk() {
+		String message = this.name + "が歩いた";
+		System.out.println(message);
+	}
+
+	public void cry() {
+		System.out.println(this.crying);
+	}
+
+	public void escape() {
+		String message = this.name + "が逃げた！";
+		System.out.println(message);
+	}
 }
--- src/zoo/AnimalApp.java
+++ src/zoo/AnimalApp.java
@@ -8,9 +8,19 @@ public class AnimalApp {
 		dog.name = "タロー";
 		dog.crying = "わん！";
 		
-		System.out.println(dog.type);
-		System.out.println(dog.name);
-		System.out.println(dog.crying);
+//		System.out.println(dog.type);
+//		System.out.println(dog.name);
+//		System.out.println(dog.crying);
+		
+		Animal cat = new Animal();
+		cat.type = "cat";
+		cat.name = "ミケ";
+		cat.crying = "にゃー！";
+				
+		dog.walk();
+		dog.cry();
+		cat.cry();
+		cat.escape();
 	}
 
 }

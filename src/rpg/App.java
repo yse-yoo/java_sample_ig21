f3943a5fbbd3782846535e03e895dd7c92722089
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-05-20 11:20:13 +0900

    add warrior

--- src/rpg/App.java
+++ src/rpg/App.java
@@ -17,6 +17,14 @@ public class App {
 		System.out.println(wizard.hp);
 		
 		wizard.walk("left");
+		
+		Character warrior = new Character();
+		warrior.name = "ボブ";
+		warrior.job = "warrior";
+		warrior.hp = 50;
+		
+		wizard.talk(warrior, "勝負しましょう！");
+		
 	}
 
 }

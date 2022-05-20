930cd0d1bf99edd958a45662b3e9554d26022beb
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-05-20 09:57:02 +0900

    add fields

--- src/rpg/App.java
+++ src/rpg/App.java
@@ -3,8 +3,11 @@ package rpg;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		//インスタンスを生成
+		Character wizard = new Character();
+		//syso を入力、Ctrl + スペース
+		System.out.println(wizard);
+		
 	}
 
 }
